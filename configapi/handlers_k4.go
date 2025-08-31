bf618e9a22cec072988430aa9cabbc13717c237e
PedroVhGit <pedrovh040110@gmail.com>
2025-08-31 17:21:56 -0400

    feat(new api): add new handlers and routes to create a CRUD for k4 keys operations

---
+++ configapi/handlers_k4.go
@@ -0,0 +1,30 @@
+package configapi
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// HandleGetsK4 handles GET /k4opt
+func HandleGetsK4(c *gin.Context) {
+	// TODO: implement
+}
+
+// HandleGetK4 handles GET /k4opt/:idsno
+func HandleGetK4(c *gin.Context) {
+	// TODO: implement
+}
+
+// HandlePostK4 handles POST /k4opt
+func HandlePostK4(c *gin.Context) {
+	// TODO: implement
+}
+
+// HandlePutK4 handles PUT /k4opt/:idsno
+func HandlePutK4(c *gin.Context) {
+	// TODO: implement
+}
+
+// HandleDeleteK4 handles DELETE /k4opt/:idsno
+func HandleDeleteK4(c *gin.Context) {
+	// TODO: implement
+}
