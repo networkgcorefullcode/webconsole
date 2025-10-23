3ea4595741aabaf9a54291c2ce1c3696742a8f75
PedroVhGit <pedrovh040110@gmail.com>
2025-10-23 16:52:14 -0400

    fix error in sync.Mutex that never locked

--- backend/ssm_sync/sync_main_functions.go
+++ backend/ssm_sync/sync_main_functions.go
@@ -141,14 +141,13 @@ func SyncKeys(keyLabel, action string) {
 }
 
 func SyncUsers() {
-	SyncUserMutex.Lock()
-	defer SyncUserMutex.Unlock()
-	// Logic to synchronize users with SSM encryption user data that are not stored in SSM
-
 	coreUserSync()
 }
 
 func coreUserSync() {
+	SyncUserMutex.Lock()
+	defer SyncUserMutex.Unlock()
+
 	if readStopCondition() {
 		logger.AppLog.Warn("The ssm is down or have a problem check if that component is running")
 		return
